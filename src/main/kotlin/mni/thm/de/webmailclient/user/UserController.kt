099de787c1ea860677dcf23d75661b83e297c7d2
Artir Guri <artir.guri@outlook.de>
2026-05-29 15:25:05 +0200

    add status codes to user controller

--- src/main/kotlin/mni/thm/de/webmailclient/user/UserController.kt
+++ src/main/kotlin/mni/thm/de/webmailclient/user/UserController.kt
@@ -5,6 +5,7 @@ import mni.thm.de.webmailclient.user.dto.UserCreate
 import mni.thm.de.webmailclient.user.dto.UserOutput
 import mni.thm.de.webmailclient.user.dto.UserUpdate
 import mni.thm.de.webmailclient.user.dto.toOutput
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
@@ -21,6 +23,7 @@ import java.util.UUID
 class UserController(private val userService: UserService) {
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun createUser(
         @Valid @RequestBody userCreate: UserCreate
     ) : UserOutput {
@@ -49,6 +52,7 @@ class UserController(private val userService: UserService) {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deleteUserById(
         @PathVariable id: UUID
     ) {
