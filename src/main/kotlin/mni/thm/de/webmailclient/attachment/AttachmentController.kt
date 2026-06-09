87ca1c6308f79fec6bab8521dfbbb65e701d1283
Artir Guri <artir.guri@outlook.de>
2026-06-09 10:07:13 +0200

    implement attachment upload

--- src/main/kotlin/mni/thm/de/webmailclient/attachment/AttachmentController.kt
+++ src/main/kotlin/mni/thm/de/webmailclient/attachment/AttachmentController.kt
@@ -1,9 +1,12 @@
 package mni.thm.de.webmailclient.attachment
 
 import mni.thm.de.webmailclient.attachment.dto.AttachmentOutput
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import java.util.UUID
@@ -13,6 +16,8 @@ import java.util.UUID
 class AttachmentController (
     private val attachmentService: AttachmentService,
 ) {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun uploadAttachment(
         @PathVariable userId: UUID,
         @PathVariable mailId: UUID,
